42de764c7ab1c0ca66fe3f500c0265ee2abe5d82
Matt Thompson <matt34thompson@gmail.com>
2017-07-27 21:06:10 +0100

    Meta tag for device width

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,6 +4,7 @@ var NavHeader = require('NavHeader');
 var Main = (props) => {
   return (
     <div>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <NavHeader/>
       <div className="row">
         <div className="columns small-10 medium-6 large-4 small-centered">
