133faa9a30e479607dfcfff0d2743de6aed019ea
Matt Thompson <vi2632@VI-LAC02VM341G8WN.broadband>
2018-05-11 19:14:08 +0100

    Added dev and prod build scripts, and webpack env variable

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-yarn run dev-server to run locally
+yarn run dev-server to run in dev
+yarn run build:dev to build (dev)
+yarn run build:prod to build (production)
+npm run serve to run live-server locally
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }, {
-      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-      loader: 'url-loader'
-    }]
-  },
-  devtool: 'cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  
+  console.log('env', env);
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }, {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader'
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
