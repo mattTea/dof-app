242bc22fd12a5e9c71098469a7aa0e87e1b512ed
Matt Thompson <matt34thompson@gmail.com>
2018-10-24 21:36:18 +0100

    WIP adds firebase config

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import getVisibleTrainingResources from './selectors/trainingResources';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import '../src/firebase/firebase';
 
 const store = configureStore();
 
