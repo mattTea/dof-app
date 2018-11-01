c36f1f21d7d934de62af3b762adabf881a46e747
Matt Thompson <matt34thompson@gmail.com>
2018-11-01 20:34:01 +0000

    Loads all course data to firebase

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { startSetTrainingResources } from './actions/trainingResources'; // import startSetTrainingResources to fetch from firebase
+import { startSetTrainingResources } from './actions/trainingResources';
 import { setTextFilter, setStageFilter, setDisciplineFilter } from './actions/filters';
 import getVisibleTrainingResources from './selectors/trainingResources';
 
@@ -16,24 +16,13 @@ const store = configureStore();
 store.subscribe(() => {
   const state = store.getState();
   const visibleTrainingResources = getVisibleTrainingResources(state.trainingResources, state.filters);
-  // console.log(visibleTrainingResources);
 });
-
-// store.dispatch(startAddTrainingItem({ title: 'BDD', description: 'Intro to BDD', stage: 'Delivery Fundamentals', discipline: 'Analysis' }));
-// store.dispatch(startAddTrainingItem({ title: 'BDD_2', description: 'How to BDD', stage: 'Individual Choice', discipline: 'Test' }));
-
-// store.dispatch(setTextFilter('how to'));
-
-// console.log(store.getState());
 
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
-
-// ReactDOM.render(<AppRouter />, document.getElementById('app'));
-// ReactDOM.render(jsx, document.getElementById('app'));
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
