73f70049aef767ef0945aeff64d19ea260b21afd
Matt Thompson <matt34thompson@gmail.com>
2018-11-04 15:18:50 +0000

    Updates loading spinner

--- src/app.js
+++ src/app.js
@@ -4,19 +4,12 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetTrainingResources } from './actions/trainingResources';
-import { setTextFilter, setStageFilter, setDisciplineFilter } from './actions/filters';
-import getVisibleTrainingResources from './selectors/trainingResources';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import '../src/firebase/firebase';
 
 const store = configureStore();
-
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleTrainingResources = getVisibleTrainingResources(state.trainingResources, state.filters);
-});
 
 const jsx = (
   <Provider store={store}>
@@ -25,8 +18,11 @@ const jsx = (
 );
 
 ReactDOM.render(
-  <div className="loading">
-    <img src={require('../public/images/smiley-spinner.png')} style={{maxWidth: "5em", maxHeight: "5em"}}></img>
+  <div className="container loading-text">
+    <div>preparing your future</div>
+    <div className="loading">
+      <img src={require('../public/images/smiley-spinner.png')} style={{maxWidth: "3em", maxHeight: "3em"}}></img>
+    </div>
   </div>,
   document.getElementById('app')
 );
